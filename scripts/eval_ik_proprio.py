4bb1fbde3f34fc96d450b266f577315bfb132a8f
ZhangXiaoxiong <12433017@mail.sustech.edu.cn>
2025-11-27 11:36:37 +0800

    modify default suite

--- scripts/eval_ik_proprio.py
+++ scripts/eval_ik_proprio.py
@@ -34,9 +34,9 @@ import torchvision
 
 # torch.cuda.empty_cache()
 
-CONFIG_PATH = "conf/eval_goal_scratch_dxr_play.yaml"
+# CONFIG_PATH = "conf/eval_goal_scratch_dxr_play.yaml"
 # CONFIG_PATH = "conf/eval_object_scratch_dxr_play.yaml"
-# CONFIG_PATH = "conf/eval_spatial_scratch_dxr_play.yaml"
+CONFIG_PATH = "conf/eval_spatial_scratch_dxr_play.yaml"
 
 class IKEvaluator:
     def __init__(self, config_path=CONFIG_PATH):
